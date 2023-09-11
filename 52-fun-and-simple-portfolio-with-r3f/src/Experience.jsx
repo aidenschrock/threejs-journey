bde5e30feed9b21ea70fc7aeff3e1b81ae0b7723
Aiden Schrock <aidennoelschrock@gmail.com>
2023-09-11 07:21:01 -0500

    complete lesson 52

--- 52-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
+++ 52-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
@@ -1,15 +1,67 @@
-import { OrbitControls } from '@react-three/drei'
+import {
+  useGLTF,
+  Environment,
+  Float,
+  PresentationControls,
+  ContactShadows,
+  Html,
+  Text,
+} from "@react-three/drei";
+import { useControls } from "leva";
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+  const computer = useGLTF(
+    "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf"
+  );
+  return (
+    <>
+      // scene
+      <color args={["#241a1a"]} attach="background" />
+      <Environment preset="sunset" />
+      // laptop
+      <PresentationControls
+        global
+        rotation={[0.13, 0.1, 0]}
+        polar={[-0.4, 0.2]}
+        azimuth={[-1, 0.75]}
+        config={{ mass: 2, tension: 400 }}
+        snap={{ mass: 4, tension: 400 }}
+      >
+        <Float rotationIntensity={0.4}>
+          <rectAreaLight
+            width={2.5}
+            height={1.65}
+            intensity={65}
+            color={"#ff6900"}
+            rotation={[-0.1, Math.PI, 0]}
+            position={[0, 0.55, -1.15]}
+          />
+          <primitive object={computer.scene} position-y={-1.2}>
+            <Html
+              transform
+              wrapperClass="htmlScreen"
+              distanceFactor={1.17}
+              position={[0, 1.56, -1.4]}
+              rotation-x={-0.256}
+            >
+              <iframe src="https://bruno-simon.com/html/" />
+            </Html>
+          </primitive>
 
-        <OrbitControls makeDefault />
-        
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
-
+          <Text
+            maxWidth={2}
+            position={[2, 0.75, 0.75]}
+            rotation-y={-1.25}
+            font="./bangers-v20-latin-regular.woff"
+            fontSize={1}
+            textAlign="center"
+          >
+            AIDEN SCHROCK
+          </Text>
+        </Float>
+      </PresentationControls>
+      //shadow
+      <ContactShadows position-y={-1.4} opacity={0.4} scale={5} blur={2.4} />
     </>
+  );
 }
